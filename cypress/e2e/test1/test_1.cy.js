62298d6155fe2acba4f51fbc4325a1b4103b61c3
Alex <sharavar.alexandr@gmail.com>
2022-08-15 10:13:41 +0300

    delete exess files

--- cypress/e2e/test1/test_1.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Log in", function() {
-    it(" Sign in", function () {
-        cy.visit("https://unsplash.com/")
-        cy.contains("Log in").click()
-        cy.get('input[type="email"]').type('testqa@ret.com')
-        cy.get('input[type="password"]').type('1234567890')
-        cy.get('input[type="submit"]').click()
-    })
-})
--- cypress/e2e/test_api_get.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Test API GET', () => {
-    it('GET Test', () => {
-        cy.request('GET', 'https://swapi.dev/api/people/1/').then((response)=>{
-            expect(response).to.have.property('status', 200)
-            expect(response.body).to.not.to.be.null
-            expect(response.body.films).to.have.length(4)
-        })
-    })
-})
